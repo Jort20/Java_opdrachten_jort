216295cee658ee7a782a3d6a822021a9f0711689
Jort20 <131657231+Jort20@users.noreply.github.com>
2024-09-17 14:20:06 +0200

    Update ConsensusSequenceCreator.java

--- src/main/java/section4_oop/part3_consensus_creator/ConsensusSequenceCreator.java
+++ src/main/java/section4_oop/part3_consensus_creator/ConsensusSequenceCreator.java
@@ -5,6 +5,8 @@
 
 package section4_oop.part3_consensus_creator;
 
+import java.util.*;
+
 /**
  *
  * @author Michiel Noback [michiel.noback@gmail.com]
@@ -16,51 +18,94 @@ public class ConsensusSequenceCreator {
     private String[] ambiguities;
     private String consensus = "";
 
-    /**
-     * testing main.
-     * @param args  the CL args
-     */
     public static void main(String[] args) {
         String[] sequences = new String[4];
         sequences[0] = "GAAT";
         sequences[1] = "GAAA";
         sequences[2] = "GATT";
         sequences[3] = "GAAC";
-        
+
         ConsensusSequenceCreator csc = new ConsensusSequenceCreator();
         String consensus;
         consensus = csc.createConsensus(sequences, true);
-        System.out.println("consensus = " + consensus);
-        //consensus should equal "GAWH"
+        System.out.println("consensus = " + consensus); // Should print "GAWH"
         consensus = csc.createConsensus(sequences, false);
-        //consensus should equal "GA[A/T][A/T/C]"
-        System.out.println("consensus = " + consensus);
+        System.out.println("consensus = " + consensus); // Should print "GA[A/T][A/C/T]"
     }
 
-    /**
-     * creates a consensus sequence from the given array of sequences.
-     * @param sequences the sequences to scan for consensus
-     * @param iupac flag to indicate IUPAC (true) or bracket notation (false)
-     * @return consensus the consensus sequence
-     */
     public String createConsensus(String[] sequences, boolean iupac) {
         this.sequences = sequences;
         this.iupac = iupac;
         this.ambiguities = new String[sequences[0].length()];
-        //in this first step, all possibilities for each position are collected
         buildAmbiguities();
-        //in this step, variants for each position are translated into either
-        //[x/y] notation or IUPAC notation
         buildConsensus();
         return this.consensus;
     }
 
-    private void buildConsensus() {
-        //YOUR CODE
-    }
-
     private void buildAmbiguities() {
-        //YOUR CODE
+        int length = sequences[0].length();
+        for (int i = 0; i < length; i++) {
+            Set<Character> uniqueNucleotides = new HashSet<>();
+            for (String seq : sequences) {
+                uniqueNucleotides.add(seq.charAt(i));
+            }
+            List<Character> sortedNucleotides = new ArrayList<>(uniqueNucleotides);
+            Collections.sort(sortedNucleotides);
+            StringBuilder sb = new StringBuilder();
+            for (char nucleotide : sortedNucleotides) {
+                sb.append(nucleotide);
+            }
+            ambiguities[i] = sb.toString();
+        }
     }
 
+    private void buildConsensus() {
+        StringBuilder sb = new StringBuilder();
+        for (String ambiguity : ambiguities) {
+            if (ambiguity.length() == 1) {
+                sb.append(ambiguity);
+            } else {
+                if (iupac) {
+                    sb.append(getIUPACCode(ambiguity));
+                } else {
+                    String sortedNucleotides = sortNucleotides(ambiguity);
+                    sb.append('[').append(String.join("/", sortedNucleotides.split(""))).append(']');
+                }
+            }
+        }
+        this.consensus = sb.toString();
+    }
+    private String sortNucleotides(String nucleotides) {
+        char[] chars = nucleotides.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+    private String getIUPACCode(String nucleotides) {
+        switch (nucleotides) {
+            case "AC":
+                return "M";
+            case "AG":
+                return "R";
+            case "AT":
+                return "W";
+            case "CG":
+                return "S";
+            case "CT":
+                return "Y";
+            case "GT":
+                return "K";
+            case "ACG":
+                return "V";
+            case "ACT":
+                return "H";
+            case "AGT":
+                return "D";
+            case "CGT":
+                return "B";
+            case "ACGT":
+                return "N";
+            default:
+                return nucleotides;
+        }
+    }
 }
