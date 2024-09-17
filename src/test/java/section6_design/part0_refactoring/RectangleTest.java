b82078c9dc9eb0a710288dac496b6e2b812c1e5c
Jort20 <131657231+Jort20@users.noreply.github.com>
2024-09-17 15:21:53 +0200

    Update RectangleTest.java

--- src/test/java/section6_design/part0_refactoring/RectangleTest.java
+++ src/test/java/section6_design/part0_refactoring/RectangleTest.java
@@ -1,15 +1,23 @@
 package section6_design.part0_refactoring;
 
 import org.junit.jupiter.api.Test;
-import section6_design.part0_refactoring.Rectangle.Point;
-import section6_design.part0_refactoring.Rectangle.View;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RectangleTest {
     private static final double DELTA = 1e-15;
     @Test
     public void getCenter() throws Exception {
-        View v = new View();
+        View v = new View() {
+            @Override
+            public void redraw() {
+
+            }
+
+            @Override
+            public void setRectangle(Rectangle rectangle) {
+
+            }
+        };
         Rectangle r = new Rectangle(v, 0, 0, 10, 20);
         Point observedP = r.getCenter();
         Point expectedP = new Point(5, 10);
@@ -19,7 +27,17 @@ public class RectangleTest {
 
     @Test
     public void contains() throws Exception {
-        View v = new View();
+        View v = new View() {
+            @Override
+            public void redraw() {
+
+            }
+
+            @Override
+            public void setRectangle(Rectangle rectangle) {
+
+            }
+        };
         Rectangle innerR = new Rectangle(v, 5, 8, 10, 20);
         Rectangle outerR = new Rectangle(v, 4, 7, 11, 21);
         assertTrue(outerR.contains(innerR));
@@ -28,7 +46,17 @@ public class RectangleTest {
 
     @Test
     public void accommodate() throws Exception {
-        View v = new View();
+        View v = new View() {
+            @Override
+            public void redraw() {
+
+            }
+
+            @Override
+            public void setRectangle(Rectangle rectangle) {
+
+            }
+        };
         Rectangle innerR = new Rectangle(v, 5, 8, 10, 20);
         Rectangle outerR = new Rectangle(v, 4, 7, 11, 21);
 
